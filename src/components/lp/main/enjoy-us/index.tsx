ed302d4eb93b2c3f3941335cc572552226927945
Vitchenzo2812 <gvitchenzo@gmail.com>
2022-09-01 10:40:42 -0300

    feat: second draft for enjoy us page

--- src/components/lp/main/enjoy-us/index.tsx
+++ src/components/lp/main/enjoy-us/index.tsx
@@ -1,27 +1,56 @@
+import { useState } from "react";
 import Button from "../../button";
 import Section from "../../section";
 import * as S from "./styled";
 
+  type Content = {
+    title: string,
+    description: string,
+  };
+
+  const data: { [key: string]: Content } = {
+    security: {
+      title: "Segurança",
+      description: "Descrição Segurança",
+    },
+    time: {
+      title: "Agilidade",
+      description: "Descrição Agilidade",
+    },
+    solve: {
+      title: "Confiabilidade",
+      description: "Descrição Confiabilidade",
+    },
+  }
+
 export default function EnjoyUs() {
+  const [activeItemIndex, setActiveItemIndex] = useState<number>(0)
+
+  const firstActiveItem = activeItemIndex === 0;
+  const secondActiveItem = activeItemIndex === 1;
+  const thirdActiveItem = activeItemIndex === 2;
+
   return (
     <Section background="url(/assets/enjoy-us-background.svg)">
-      <S.Title>Serviços de qualidade, para todas as suas requisições</S.Title>
-      <S.Cards>
-        <S.Card>
-          <S.CardIcon icon="/assets/security-icon.svg" />
-        </S.Card>
-        <S.Card>
-          <S.CardIcon icon="/assets/time-icon.svg" />
-        </S.Card>
-        <S.Card>
-          <S.CardIcon icon="/assets/solve-icon.svg" />
-        </S.Card>
-      </S.Cards>
-      <S.ServiceDescription>
-        <h1>Título</h1>
-        <p>Descrição</p>
-        <Button label="Cadastrar" variant="secondary" />
-      </S.ServiceDescription>
+      <S.Wrapper>
+        <S.Title>Serviços de qualidade, para todas as suas requisições</S.Title>
+        <S.Cards>
+          <S.Card>
+            <S.CardIcon icon="/assets/security-icon.svg" />
+          </S.Card>
+          <S.Card>
+            <S.CardIcon icon="/assets/time-icon.svg" />
+          </S.Card>
+          <S.Card>
+            <S.CardIcon icon="/assets/solve-icon.svg" />
+          </S.Card>
+        </S.Cards>
+        <S.ServiceDescription>
+          <h1>Título</h1>
+          <p>Descrição</p>
+          <Button label="Cadastrar" variant="secondary" />
+        </S.ServiceDescription>
+      </S.Wrapper>
     </Section>
   );
 }
--- src/components/lp/main/enjoy-us/styled.ts
+++ src/components/lp/main/enjoy-us/styled.ts
@@ -1,25 +1,48 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    row-gap: 3.9rem;
+`
+
 export const Title = styled.h1`
     font-weight: 700;
     font-size: 2.5rem;
+    text-align: center;
+    width: 100%;
 `
 
 export const Cards = styled.div`
     width: 100%;
     height: max-content;
     display: flex;
+    justify-content: space-around;
     flex-direction: row;
-    column-gap: 6rem;
 `
 
 export const Card = styled.div`
+    cursor: pointer;
     width: 33.8rem;
     height: 27rem;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border: 1px solid #000;
     border-radius: 8px;
     background-color: #fff;
+    display: flex;
+    align-items: center;
+    transition: all .3s ease-in-out;
+    justify-content: center;
+    :hover {
+        transform: scale(1.02);
+        box-shadow: 0px 6px 4px 3px var(--pink);
+        border: 1px solid var(--pink);
+        span {
+            background-color: var(--pink);
+        }
+    }
 `
 
 export const CardIcon = styled.span<{ icon: string }>`
@@ -29,6 +52,7 @@ export const CardIcon = styled.span<{ icon: string }>`
     mask-size: auto;
     mask-position: center;
     background-color: #000;
+    transition: all .3s ease-in-out;
     width: 80%;
     height: 80%;
 `
