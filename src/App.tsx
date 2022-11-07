955d99f8160f312bfba9632d2e4d304824fc644e
FelipeDeluca212 <felipedeluca.fd@gmail.com>
2022-11-07 11:45:29 -0300

    resolving changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import LandingPage from "./components/lp";
 import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn/SignIn";
 import { GlobalStyles } from "./styles/global";
 
 export function App() {
   return (
     <>
       <GlobalStyles />
+      <LandingPage />
       <SignUp />
+      <SignIn />
     </>
   );
 }
--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 import { Body } from "../../components/signin/Body/Body"
-import { Footer } from "../../components/Footer/Footer"
-import { HeaderCadastro } from "../../components/Header/Header"
+import { Footer } from "../../components/signin/Footer/Footer"
+import { HeaderCadastro } from "../../components/signin/Header/Header"
 
 export default function SignIn() {
 
