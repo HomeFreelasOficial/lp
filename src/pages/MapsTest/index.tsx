dca263ff8cdf6e06f2a4fb2035f1a9218b182d47
PedroBrandao05 <pedro-brandao2012@hotmail.com>
2022-11-08 21:52:57 -0300

    feat: geolocation getting current user location

--- src/pages/MapsTest/index.tsx
+++ src/pages/MapsTest/index.tsx
@@ -1,26 +1,54 @@
 import styled from "styled-components";
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
-
+import React, { useState, useEffect } from 'react';
 import './style.css';
+import { Wrapper } from "./styles";
 
 
 export interface MapPageProps {}
 
+
+
+
 export default function Mapa() {
 
-  const Wrapper = styled.div`
-    height: 100vh;
-  `
+  const [position, setPosition] = useState({
+    lat: 43.0949969,
+    lng: 12.310112
+  })
+
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
+  function success(pos: GeolocationPosition) {
+    const crd = pos.coords;
+
+    setPosition({
+      lat: crd.latitude,
+      lng: crd.longitude
+    })
+  }
+
+  function error(err: GeolocationPositionError) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+  
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      success, error, options
+    )
+  })
 
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyBJ-k9DL9UE8blCP9dsv8v0y3yykuCN55U"
   })
 
-  const position = {
-    lat: -23.5314183,
-    lng: -46.4489193
-  }
+ 
 
   return (
     <Wrapper>
@@ -30,10 +58,18 @@ export default function Mapa() {
               mapContainerStyle={{width: '100%', height: '100%'}}
               center={position}
               zoom={15}
+              options={
+                {
+                  zoomControl: false,
+                  streetViewControl: false,
+                  mapTypeControl: false,
+                  fullscreenControl: false
+                }
+              }
             >
               <Marker position={position} options={{
                 label: {
-                  text: "José Nefasto está aqui te esperando para contar um segredo",
+                  text: "Você está aqui",
                   className: 'map-marker'
                 }
               }}/>
