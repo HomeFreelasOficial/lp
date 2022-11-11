0d957f87a48de35b6fbd460037ce6eeb78bf5e36
Vitchenzo2812 <gvitchenzo@gmail.com>
2022-11-11 08:16:07 -0300

    chore: footer lp

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,28 +1,32 @@
 import styled from "styled-components"
-export const FooterComponent = styled.footer`
 
-  width: auto;
+
+export const FooterComponent = styled.footer`
   display: flex;
   align-items: center;
   justify-content: center;
 
   gap: 1.7rem;
   background-color: #222628;
-  padding: 2em;
+  padding: 2rem;
  
   @media screen and (min-width: 768px){
-   padding: 1em;
+   padding: 5rem;
   }
  
   @media screen and (min-height: 768px){
-   gap: 1.75em;
+   gap: 1.75rem;
   }
  
   @media screen and (min-width: 1366px){
-   padding: 2em;
+   padding: 3rem;
   }
 `
 export const Text = styled.p`
 color: white;
 font-size: 12px;
+
+@media screen and (min-width: 768px){
+  font-size: 24px;
+}
 `
